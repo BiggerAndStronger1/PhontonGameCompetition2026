3f22069660c9aec33d817d5ffc9215c9683ba45e
Yucong He <hyc018018@gmail.com>
2026-03-25 12:09:47 +1100

    scriptable object

---
+++ Assets/Scripts/SOScript/AudioSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+[CreateAssetMenu(fileName = "AudioSO", menuName = "Scriptable Objects/AudioSO")]
+public class AudioSO : ScriptableObject
+{
+    public List<AudioClip> clipList;
+}
