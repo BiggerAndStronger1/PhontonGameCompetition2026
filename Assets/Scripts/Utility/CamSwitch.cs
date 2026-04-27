8952e5d7ccbf4a8bd7f4ffcea90dad33df4e9bfb
Yucong He <hyc018018@gmail.com>
2026-04-27 21:42:01 +1000

    bugfix in camSwitch

--- Assets/Scripts/Utility/CamSwitch.cs
+++ Assets/Scripts/Utility/CamSwitch.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CamSwitch : MonoBehaviour
 {
-    private CinemachineCamera cam;
+    [SerializeField]private CinemachineCamera cam;
     private void Awake()
     {
-        cam = GetComponentInParent<CinemachineCamera>();
+       
     }
 
     public void ForcedAwake()
@@ -29,13 +29,23 @@ public class CamSwitch : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Player>() != null && cam)
+        {
             cam.Priority = 1;
+            cam.Follow = GameObject.FindGameObjectWithTag("Player").transform;
+            cam.enabled = true;
+        }
+        
 
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.GetComponent<Player>() != null && cam)
+        {
             cam.Priority = 0;
+            cam.enabled = false;
+            cam.transform.position = Vector2.zero;
+        }
+           
     }
 }
