6b516552a0c8a416d7eda87ddc443b92e889401f
TsukiXinyue <143994593+TsukiXinyue@users.noreply.github.com>
2026-04-29 15:51:02 +0100

    关卡1终

--- Assets/Scripts/ButtonsAndDoors/TouchTriggerButton.cs
+++ Assets/Scripts/ButtonsAndDoors/TouchTriggerButton.cs
@@ -21,19 +21,22 @@ public class TouchTriggerButton : Button
     }
 
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if ((collision.CompareTag("Player") || collision.CompareTag("Enemy")) && needLongPress)
-        {
-            if (pressNumber > 0)
-                pressNumber--;
-        }
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (needLongPress)
+		{
+			if (collision.CompareTag("Player") || collision.CompareTag("Enemy"))
+			{
+				if (pressNumber > 0)
+					pressNumber--;
+			}
 
-        if (pressNumber == 0)
-            UnpressButton();
-    }
+			if (pressNumber == 0)
+				UnpressButton();
+		}
+	}
 
-    protected override void ResetButton()
+	protected override void ResetButton()
     {
         base.ResetButton();
         pressNumber = 0;
