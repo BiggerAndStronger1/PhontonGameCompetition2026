e30d548fa4d697f2c68d30cf3cf6429c53113118
lvboluo24 <1259918933@qq.com>
2026-05-05 04:42:17 +0800

    更新第四关

--- Assets/Scripts/Ground/TeleportEdge.cs
+++ Assets/Scripts/Ground/TeleportEdge.cs
@@ -13,10 +13,11 @@ public class TeleportEdge : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
+        
         if (Index==0)
         {
             Vector3 deltaPosition = targetTeleportEdge.transform.position - transform.position + targetTeleportEdge.offset;
-
         EventManager2P<Transform, Vector3>.TriggerEvent(
             GameEvents.Teleport,
             collision.transform,
@@ -27,20 +28,22 @@ public class TeleportEdge : MonoBehaviour
         {
             //移动player到目标位置
             //记录player的位置
-            Vector3 playerPosition = player.transform.position;
-            player.transform.position = targetTeleportEdge.transform.position + targetTeleportEdge.offset;
+            Vector3 playerPosition = collision.transform.position;
+            collision.transform.position = targetTeleportEdge.transform.position + targetTeleportEdge.offset;
             //player的x轴位置保持不变
-            player.transform.position = new Vector3(playerPosition.x, player.transform.position.y, player.transform.position.z);
+            collision.transform.position = new Vector3(playerPosition.x, collision.transform.position.y, collision.transform.position.z);
         }
         if (Index==2)
         {
             //移动player到目标位置
             //记录player的位置
-            Vector3 playerPosition = player.transform.position;
-            player.transform.position = targetTeleportEdge.transform.position + targetTeleportEdge.offset;
+            Vector3 playerPosition = collision.transform.position;
+            collision.transform.position = targetTeleportEdge.transform.position + targetTeleportEdge.offset;
             //player的y轴位置保持不变
-            player.transform.position = new Vector3(player.transform.position.x, playerPosition.y, player.transform.position.z);
+            collision.transform.position = new Vector3(collision.transform.position.x, playerPosition.y, collision.transform.position.z);
         }
+        
+        
         
     }
 
