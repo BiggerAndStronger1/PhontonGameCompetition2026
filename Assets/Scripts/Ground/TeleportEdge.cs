9b747a25ea51be58f26a5051be6890bbba846dc1
lvboluo24 <1259918933@qq.com>
2026-05-05 04:04:27 +0800

    上传第四关，修改传送方块bug

--- Assets/Scripts/Ground/TeleportEdge.cs
+++ Assets/Scripts/Ground/TeleportEdge.cs
@@ -4,16 +4,44 @@ public class TeleportEdge : MonoBehaviour
 {
     public TeleportEdge targetTeleportEdge;
     public Vector3 offset;
+        [SerializeField] private Player player;
+        public int Index;//1，上下，2，左右
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Vector3 deltaPosition = targetTeleportEdge.transform.position - transform.position + targetTeleportEdge.offset;
+        if (Index==0)
+        {
+            Vector3 deltaPosition = targetTeleportEdge.transform.position - transform.position + targetTeleportEdge.offset;
 
         EventManager2P<Transform, Vector3>.TriggerEvent(
             GameEvents.Teleport,
             collision.transform,
             deltaPosition
         );
+        }
+        if (Index==1)
+        {
+            //移动player到目标位置
+            //记录player的位置
+            Vector3 playerPosition = player.transform.position;
+            player.transform.position = targetTeleportEdge.transform.position + targetTeleportEdge.offset;
+            //player的x轴位置保持不变
+            player.transform.position = new Vector3(playerPosition.x, player.transform.position.y, player.transform.position.z);
+        }
+        if (Index==2)
+        {
+            //移动player到目标位置
+            //记录player的位置
+            Vector3 playerPosition = player.transform.position;
+            player.transform.position = targetTeleportEdge.transform.position + targetTeleportEdge.offset;
+            //player的y轴位置保持不变
+            player.transform.position = new Vector3(player.transform.position.x, playerPosition.y, player.transform.position.z);
+        }
+        
     }
 
     private void OnDrawGizmos()
