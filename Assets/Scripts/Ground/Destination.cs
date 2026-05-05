96c431f5c5648fd1e59161609bfb9c7d59242a23
Yucong He <hyc018018@gmail.com>
2026-05-05 21:54:55 +1000

    premerge

--- Assets/Scripts/Ground/Destination.cs
+++ Assets/Scripts/Ground/Destination.cs
@@ -23,7 +23,6 @@ public class Destination : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")){
-            print("level completed");
             EventManagerNP.TriggerEvent(GameEvents.LoadNextScene);
         }
     }
