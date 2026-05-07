e79be3b61bac65b03c0f7da61ff25cc31c20d115
Yucong He <hyc018018@gmail.com>
2026-05-07 10:32:23 +1000

    bugfix in air trap

--- Assets/Scripts/Ground/SwitchButton.cs
+++ Assets/Scripts/Ground/SwitchButton.cs
@@ -18,6 +18,9 @@ public class SwitchButton : MonoBehaviour
     [SerializeField] private Color notPressedColor = Color.red;
     [Tooltip("color when the button is pressed")]
     [SerializeField] private Color pressedColor = Color.green;
+    [SerializeField] private PropType propType;
+    [SerializeField] private int quantity;
+    private bool unlocked;
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -35,17 +38,23 @@ public class SwitchButton : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Player") && !pressed)
+        if (other.gameObject.CompareTag("Player") && !pressed && Condition())
         {
             EventManager1P<GameObject>.TriggerEvent(notPressesEvent, transform.parent.gameObject);
             spriteRenderer.color = pressedColor;
             pressed = true;
         }
-        else
+        else if (other.gameObject.CompareTag("Player") && pressed && Condition())
         {
             EventManager1P<GameObject>.TriggerEvent(pressedEvent, transform.parent.gameObject);
             spriteRenderer.color = notPressedColor;
             pressed = false;
         }
+    }
+
+    private bool Condition()
+    {
+        if (!unlocked) unlocked = EventManagerReturn1P<PropType, int>.TriggerEvent(GameEvents.InventoryQuery, propType) >= quantity;
+        return unlocked;
     }
 }
